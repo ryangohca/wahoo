aec639d49f12bffc430464da6172e6e075e69e95
ryangohca <ryan.gohca2006@gmail.com>
2024-05-02 22:22:48 +0800

    create post html done

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, HiddenField
-from wtforms.validators import InputRequired, ValidationError
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, PasswordField, BooleanField, HiddenField, IntegerField, TextAreaField, SelectMultipleField
+from wtforms.validators import InputRequired, ValidationError, NumberRange, Optional
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cutechodan" # yeah not supposed to have secret key in source code but who cares
@@ -22,6 +23,22 @@ class LoginForm(FlaskForm):
         password = field.data.strip()
         pass
 
+class UploadForm(FlaskForm):
+    photo = FileField('Image', validators=[
+        Optional(),
+        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+    ])
+    rate = IntegerField("Rate", validators=[InputRequired(), NumberRange(min=1, max=5)])
+    description = TextAreaField("Description", validators=[InputRequired()])
+    tags = SelectMultipleField(
+        'Tags (ctrl-click to select multiple tags):',
+        validators=[Optional()],
+        choices=[
+            ('Western', 'Western'),
+            ('Japanese', 'Japanese'),
+            ('Chicken Rice', 'Chicken Rice')
+        ])
+
 @app.route('/')
 def root():
     return render_template("home.html")
@@ -29,6 +46,10 @@ def root():
 @app.route('/login')
 def login():
     return render_template('login.html', loginForm=LoginForm())
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html', uploadForm=UploadForm())
 
 if __name__ == '__main__':
     app.run(debug=True) #to remove after everything is done
